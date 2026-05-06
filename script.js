f0847ecfd8333f0bd36658616950fd99d1502760
aitchabal loubna <aitchableloubna@gmail.com>
2026-05-06 12:42:18 +0100

    Refactor navigation item click event handling

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 lucide.createIcons();
 
-// GESTION DU CARRÉ BLEU DYNAMIQUE
 const navItems = document.querySelectorAll('.nav-item');
 
 navItems.forEach(item => {
     item.addEventListener('click', function() {
-        // Enlever la classe active de TOUS les liens
         navItems.forEach(nav => nav.classList.remove('active'));
-        // Ajouter la classe active seulement à celui cliqué
         this.classList.add('active');
     });
 });
