0bd050681d1933bc58767b16a56a5b2c53e930ec
aitchabal loubna <aitchableloubna@gmail.com>
2026-05-06 12:20:29 +0100

    Enhance comments in script.js for navigation handling
    Updated comments for better clarity and organization.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 lucide.createIcons();
 
-// Gestion dynamique des onglets
+// GESTION DU CARRÉ BLEU DYNAMIQUE
 const navItems = document.querySelectorAll('.nav-item');
+
 navItems.forEach(item => {
     item.addEventListener('click', function() {
+        // Enlever la classe active de TOUS les liens
         navItems.forEach(nav => nav.classList.remove('active'));
+        // Ajouter la classe active seulement à celui cliqué
         this.classList.add('active');
     });
 });
